77027a345a2d172afee5c1f4b96d23dcfad7732a
TTaannaakkaa <ee203081@meiji.ac.jp>
2023-08-30 13:19:38 +0900

    add comments

--- obstacle_detector/src/obstacle_detector_node.cpp
+++ obstacle_detector/src/obstacle_detector_node.cpp
@@ -1,5 +1,12 @@
 #include "obstacle_detector/obstacle_detector.h"
 
+/**
+ * @brief main function 
+ * 
+ * @param argc 
+ * @param argv 
+ * @return int 
+ */
 int main(int argc, char** argv)
 {
     ros::init(argc, argv, "obstacle_detector");
